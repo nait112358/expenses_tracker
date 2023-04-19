83482ac55a52e9bf09e9100f7651ef38a9c166c1
bastian <bastian.barrera.r@gmail.com>
2023-04-18 20:41:08 -0400

    done, finished styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,5 @@
 let expenses = [];
-let account = [0, 0, 0 ]; // initial, expenses, total
-let balanceTotal = account[0];
-let expensesTotal = account[1];
-let initialBalance = account[2];
+let account = [0, 0, 0]; // initial, expenses, total
 //getting and validating budget
 document.getElementById("budget-btn").addEventListener("click", (e) => {
   e.preventDefault();
@@ -13,7 +10,7 @@ document.getElementById("budget-btn").addEventListener("click", (e) => {
     document.getElementById("budget-card").reset();
     account[0] = initialBudget;
     account[2] = initialBudget;
-    console.log(account)
+    console.log(account);
   } else {
     alert("solo puede ingresar numeros enteros positivos");
   }
@@ -38,7 +35,7 @@ document.getElementById("expenses-btn").addEventListener("click", (e) => {
     document.getElementById("expenses-card").reset();
     expensesTracker(expenses);
     balanceTracker(account[1]);
-    console.log(account)
+    console.log(account);
   } else {
     alert("solo puede ingresar numeros enteros positivos");
   }
@@ -50,7 +47,7 @@ const expensesTracker = (arr) => {
     .reduce((a, b) => a + b, 0);
   document.getElementById("general-expenses").innerText = `$ ${sumExpenses}`;
   account[1] = sumExpenses;
-  console.log(account)
+  console.log(account);
 };
 
 // updating balance after expense its passed
@@ -58,7 +55,7 @@ const balanceTracker = (expenses) => {
   let total = account[0] - expenses;
   account[2] = total;
   document.getElementById("general-budget").innerText = `$ ${account[2]}`;
-  console.log(account)
+  console.log(account);
 };
 
 //create random ID for my objects without repeating itself
@@ -83,7 +80,7 @@ const createRow = (kind, amount, id) => {
   //creating articles
   let articles = `<article class="expenses-table-kind">${kind}</article>
     <article class="expenses-table-expenses" id="b${id}">$ ${amount}</article>
-    <article class="expenses-table-bin"><a href="#" class="text-decoration-none" id="a${id}" onCLick="binBtn(this.id);">
+    <article class="expenses-table-bin pt-2"><a href="#" class="text-decoration-none" id="a${id}" onCLick="binBtn(this.id);">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" id="bin${id}" class="bi bi-trash-fill" viewBox="0 0 16 16">
     <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"></path>
     </svg></a></article>`;
@@ -97,15 +94,15 @@ const createRow = (kind, amount, id) => {
 const binBtn = (clicked_id) => {
   let gettedID = clicked_id.match(/\d+/g);
   let idForUse = gettedID[0];
-  let idForDelete = `b${idForUse}`
-  let properID = `identifier${idForUse}`
+  let idForDelete = `b${idForUse}`;
+  let properID = `identifier${idForUse}`;
   binTracker(idForDelete, account[2], account[1]); //updating balance and expenses
   removeObj(expenses, idForUse);
-  console.log(expenses)
+  console.log(expenses);
   document.getElementById(properID).outerHTML = "";
-}
+};
 //updating balance after bin is used
-function binTracker(id, initialBudget, sumExpenses){
+function binTracker(id, initialBudget, sumExpenses) {
   let tracker1 = document.getElementById(id).innerText;
   let tracker2 = tracker1.match(/\d+/g);
   account[2] = account[2] + parseInt(tracker2[0]);
@@ -115,12 +112,11 @@ function binTracker(id, initialBudget, sumExpenses){
 }
 
 //removing object targeted in bin
- const removeObj = (arr,idNumb) =>{
-  console.log(idNumb)
-  let index = arr.findIndex(object => {
+const removeObj = (arr, idNumb) => {
+  console.log(idNumb);
+  let index = arr.findIndex((object) => {
     return object.id == idNumb;
-  })
-  arr.splice(index, 1)
+  });
+  arr.splice(index, 1);
   return arr;
-}
-
+};
